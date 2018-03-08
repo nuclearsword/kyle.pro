7ee5811bd213f6b252ff29444f6b4ea4c8f97ed6
Kyle Dunbar <kylevdunbar@gmail.com>
2018-03-07 22:05:55 -0700

    working on a card homepage

--- src/App.js
+++ src/App.js
@@ -5,14 +5,22 @@ import './App.css';
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+      // <div className="App">
+      //   <header className="App-header">
+      //     <img src={logo} className="App-logo" alt="logo" />
+      //     <h1 className="App-title">Welcome to React</h1>
+      //   </header>
+      //   <p className="App-intro">
+      //     To get started, edit <code>src/App.js</code> and save to reload.
+      //   </p>
+      // </div>
+      <div class="container">
+        <div class="card mx-auto">
+          <div class="card-body">
+
+          </div>
+        </div>
+
       </div>
     );
   }
